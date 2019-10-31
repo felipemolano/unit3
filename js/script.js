3f3ab72ab912d39256ba939ba04bfff33e2fe311
felipemolano <51757650+felipemolano@users.noreply.github.com>
2019-10-31 13:56:41 -0500

    beta

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-
-
-
 $("#name").focus();
 $("#other-title").hide();
 $("#design").children().eq(0).hide();
 $("#color").val("please select design");
 
-
+////////////////////////////////////////////////////////////  event that check Design dropdown changes
 $("#design").on('change', function(e){
 
  console.log(this.value);
+ const $colorChildren = $("#color").children();
  if(this.value ==="js puns")
  {
-    const $colorChildren = $("#color").children();
+    
     $.each($colorChildren, function(index,value){
 
         const str = value.innerText;
@@ -34,7 +32,6 @@ $("#design").on('change', function(e){
  else{
 
 
-    const $colorChildren = $("#color").children();
     $.each($colorChildren, function(index,value){
 
         const str = value.innerText;
@@ -53,7 +50,16 @@ $("#design").on('change', function(e){
     })
 
  }
-   
-
-
 });
+
+let $cost = 0.0;
+const $activities = $(".activities").append("Cost : " + $cost);
+
+$(".activities").on("change","input",function(e){
+
+console.log(parseInt($(this).attr("data-cost").substring(1)));
+console.log($(this).attr("data-day-and-time"));
+//$cost += parseInt($(this).attr("data-cost"));
+console.log($cost);
+});
+
