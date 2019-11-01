b06a292993631129f92ff933179976ebfc9514b2
felipemolano <51757650+felipemolano@users.noreply.github.com>
2019-11-01 14:26:20 -0500

    update 1.2
    validation remains only

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 $("#name").focus();
 $("#other-title").hide();
 $("#design").children().eq(0).hide();
-$("#color").val("please select design");
+const $defaultOption = $("#color").append("<option id='temp' value='default'>please select a T-shirt theme</option>")
+const $firstElement = $("#color").children().eq(0).text();
+$("#color").children().eq(0).text("Please select a T-shirt theme");
+
+$("#color").children().hide();
 
 ////////////////////////////////////////////////////////////  event that check Design dropdown changes
 $("#design").on('change', function(e){
 
- console.log(this.value);
+$("#color").children().eq(0).text($firstElement);
+$("#temp").remove();
+ 
  const $colorChildren = $("#color").children();
- console.log($colorChildren);
+ 
  if(this.value ==="js puns")
  {
     
-    $.each($colorChildren, function(index,value){
+    $colorChildren.each( function(index,value){
 
         const str = value.innerText;
-
-        if(str.includes("JS Puns shirt only"))
+        if(str.includes("JS Puns shirt only")  && !str.includes("please"))
         {
             $(this).show();
         }
@@ -33,7 +38,7 @@ $("#design").on('change', function(e){
  else{
 
 
-    $.each($colorChildren, function(index,value){
+    $colorChildren.each( function(index,value){
 
         const str = value.innerText;
 
@@ -56,7 +61,7 @@ $("#design").on('change', function(e){
 let $cost = 0.0;
 const $label = $('<label id="Cost">Cost : ' + $cost + '</label>');
 const $activities = $(".activities").append($label);
-console.log($(".activities label").children());
+//console.log($(".activities label").children());
 let $inputCollection = $(".activities label").children();
 
 
@@ -65,40 +70,54 @@ $(".activities ").on("change","input",function(e){
 
 let costActivity = parseInt($(this).attr("data-cost").substring(1));
 let dateActivity = $(e.target).attr("data-day-and-time");
-console.log(dateActivity);
 
 
 
-$.each($inputCollection, function(index,value){
-
-    console.log($(this).attr('data-day-and-time'));
-    if($(this).attr('data-day-and-time') === dateActivity)
-    {
-        $( this ).prop( "disabled", true );
-    } 
-    else
-    {
-        $( this ).prop( "disabled", false);
-
-    }
-
-})
 
 
 if($(this).prop("checked")){
     $cost += costActivity;
     $("#Cost").html('<label id="Cost">Cost : '+ $cost + '</label>');
 
+    $inputCollection.each(function(index,value){
+
+        //console.log($(this).attr('data-day-and-time'));
+        let $currentElement = $(this).attr('data-day-and-time');
+        if( $currentElement === dateActivity && $(e.target).attr("name") !== $(this).attr("name") )
+        {
+            $(this).prop( "disabled", true );
+            
+            
+        } 
+      
+    
+    })
+    
+
 }else {
     $cost -= costActivity;
     $("#Cost").html('<label id="Cost">Cost : '+ $cost + '</label>');
+    $inputCollection.each(function(index,value){
+
+       
+        let $currentElement = $(this).attr('data-day-and-time');
+        if( $currentElement === dateActivity && $(e.target).attr("name") !== $(this).attr("name") )
+        {
+            $(this).prop( "disabled", false );
+            
+            
+        } 
+      
+    
+    })
+    
 }
 
 
 
 
-console.log(parseInt($(this).attr("data-cost").substring(1)));
+//console.log(parseInt($(this).attr("data-cost").substring(1)));
 //console.log($(this).attr("data-day-and-time"));
 //$cost += parseInt($(this).attr("data-cost"));
-console.log($cost);
+//console.log($cost);
 });
